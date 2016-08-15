f60521af1f543af236065820890be96bdf147c71
leotok <leo.wajnsztok@gmail.com>
2016-08-15 00:41:48 -0300

    ace code editor ok

--- mydjangowebsite/static/assets/js/blog.js
+++ mydjangowebsite/static/assets/js/blog.js
@@ -1,6 +1,9 @@
 var i = 0;
 
 $(function() {
+
+	// Ace - https://ace.c9.io/#nav=howto
+
 	$(".editor").each(function() {
 
 		ace.require("ace/ext/language_tools");
@@ -10,7 +13,8 @@ $(function() {
 	    editor.getSession().setMode("ace/mode/python");
 	    editor.setOptions({
 	        enableBasicAutocompletion: true,
-	        enableSnippets: true
+	        enableSnippets: true,
+	        maxLines: Infinity
 	  	});
 	  
 	  	editor.setShowPrintMargin(false);
